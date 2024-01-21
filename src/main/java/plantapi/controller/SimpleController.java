d8a4bf56c8c8c102b21a5a97a1ee3fbfc57062be
Dennis Kasch <dennis-kasch@web.de>
2024-01-21 16:35:54 +0100

    implement plant analysis endpoint

--- src/main/java/plantapi/controller/SimpleController.java
+++ src/main/java/plantapi/controller/SimpleController.java
@@ -21,12 +21,40 @@ public class SimpleController {
     public String describeImage(
         // define necessary parameters for the endpoint
         @RequestParam(value = "userMessage") String userMessage,
-        @RequestParam(value = "imageUrl") String imageUrl,
-        @RequestParam(value = "tokenLimit") String tokenLimit
+        @RequestParam(value = "imageUrl") String imageUrl
     ) 
     {
         // get an answer from chat gpt
-        String answer = connection.getChatGPTAnswer(userMessage, imageUrl, tokenLimit);
+        String answer = connection.getChatGPTAnswer("empty", userMessage, imageUrl, "300");
+        System.out.println(answer);
+        return answer;
+    }
+
+    @GetMapping("/analysePlantImage")
+    public String analysePlantImage(
+        // define necessary parameters for the endpoint
+        @RequestParam(value = "imageUrl") String imageUrl
+    ) 
+    {
+        String systemMessage= """
+            When I send you an image please check if it is an image of a plant or a part of a plant. If not, reply exactly with 'ERROR: This image is no plant or part of a plant.' and nothing else.
+            If the image is a plant or part of a plant, please give the name of the plant in the format 'plant name: [NAME OF THE PLANT]'. Answer 'plant name: unklnown' if you cannot identify it.
+            In addition, please identify if this plant is healthy or if it has any diseases. Answer with 'health status: healthy' or 'health status: unhealthy' according to the healh status of the plant.
+            If it has any diseases, please give an answer of the format 'diseases: [DISEASE_1, DISEASE_2, DISEASE_3, ..., DISEASE_N]'.
+            Put each answer in its own line and seperate them with a comma.
+        """;
+        systemMessage = systemMessage.replaceAll("\n", "\\\\n");
+        //String systemMessage="Please identify if the image is a plant or not and if it has any diseases.";
+        // get an answer from chat gpt
+        String answer = connection.getChatGPTAnswer(systemMessage, "empty", imageUrl, "300");
+        System.out.println(answer);
+        return answer;
+    }
+
+    @GetMapping("/")
+    public String base() 
+    {
+        String answer = "Server is running";
         System.out.println(answer);
         return answer;
     }
