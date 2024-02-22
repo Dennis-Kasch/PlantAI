72a8ad8e01ab085f7901e2e04f605799b5a72754
Dennis Kasch <dennis-kasch@web.de>
2024-02-22 10:08:33 +0100

    implement api key check and move variables to .env file

--- src/main/java/plantapi/controller/SimpleController.java
+++ src/main/java/plantapi/controller/SimpleController.java
@@ -3,18 +3,21 @@ package plantapi.controller;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 import javax.imageio.ImageIO;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import com.cloudinary.utils.StringUtils;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
@@ -28,11 +31,14 @@ public class SimpleController {
     public SimpleController() {
         // Create connection to chatGPT
         dotenv = Dotenv.configure().load();
-        this.connection = new ChatGPTConnection(dotenv.get("GPT_API_KEY"), dotenv.get("MODEL"));
         this.cloudinary = new Cloudinary(ObjectUtils.asMap(
             "cloud_name", dotenv.get("CLOUDINARY_NAME"),
             "api_key", dotenv.get("CLOUDINARY_KEY"),
             "api_secret", dotenv.get("CLOUDINARY_SECRET")));
+        this.connection = new ChatGPTConnection(dotenv.get("GPT_API_KEY"), dotenv.get("VISUAL_MODEL"));
+        String keyResponse = this.connection.checkApiKey();
+        if (keyResponse.contains("ERROR")) throw new RuntimeException("Shutting down backend due to runtime exception.");
+        else System.out.println(keyResponse);
     }
 
     @SuppressWarnings("unchecked")
@@ -62,12 +68,12 @@ public class SimpleController {
                 BufferedImage bufferedImage = ImageIO.read(image.getInputStream());
                 int width = bufferedImage.getWidth();
                 int height = bufferedImage.getHeight();
-                if (width > 800 || height > 600) {
+                if (width > Integer.parseInt(dotenv.get("IMAGE_MAX_WIDTH")) || height > Integer.parseInt(dotenv.get("IMAGE_MAX_HEIGHT"))) {
                     return "Error: Image must be 800x600 pixels or smaller";
                 }
                 else {
                     String fileName = image.getOriginalFilename();
-                    Path filePath = Paths.get(dotenv.get("UPLOAD_DIR"), fileName);
+                    Path filePath = Paths.get(dotenv.get("LOCAL_IMAGE_DIR"), fileName);
                     System.out.println("Saving image to " + filePath.toString());
                     image.transferTo(filePath.toFile());
                     System.out.println("Uploading image to filehoster");
@@ -83,6 +89,24 @@ public class SimpleController {
         }
     }
 
+    @GetMapping("/checkApiKey")
+    public String checkApiKey() {
+        return null;
+    }
+
+    private String readPromptFromFile(String filePath) {
+        String workingDir = System.getProperty("user.dir");
+        System.out.println("Current working directory: " + workingDir);
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(filePath));
+            String analysisPrompt = String.join("\n", allLines);
+            return analysisPrompt;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     @PostMapping("/analysePlantImage")
     public String analysePlantImage(
         @RequestParam(value = "image") MultipartFile image
@@ -92,17 +116,8 @@ public class SimpleController {
         if (uploadAnswer.toLowerCase().contains("error")) return uploadAnswer;
         else imageUrl = uploadAnswer.replace("Image uploaded successfully. Hosted at: ","");
         System.out.println("Analyzing "+imageUrl+" for its health status");
-        String[] promptCommands = {
-            "When I send you an image please check if it's an image of a plant or a part of a plant.",
-            "If not, reply exactly with 'ERROR: This image is no plant or part of a plant.' and nothing else.",
-            "If the image is a plant or part of a plant, please give the name of the plant in the format 'plant_name: [NAME OF THE PLANT]'.",
-            "Answer 'plant_name: unknown' if you cannot identify it.",
-            "In addition, please identify if this plant is healthy or if it has any diseases.",
-            "Answer with 'health_status: healthy' or 'health_status: unhealthy' according to the health status of the plant.",
-            "If it has any diseases, please give an answer of the format 'disease_list: [DISEASE_1, DISEASE_2, DISEASE_3, ..., DISEASE_N]'",
-        };
 
-        String systemMessage = StringUtils.join(promptCommands, " ");
+        String systemMessage = readPromptFromFile("./src/main/resources/analysisPrompt.txt");
         String gptAnswer = connection.getAnswerByUrl(systemMessage, "empty", imageUrl, "300");
         System.out.println(gptAnswer);
         return gptAnswer;
