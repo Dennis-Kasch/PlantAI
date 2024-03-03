bd3b625e46d077b2e572161422760e04f00d1d59
Dennis Kasch <dennis-kasch@web.de>
2024-03-03 12:10:22 +0100

    remove unnecessary imports

--- src/main/java/plantapi/controller/UserController.java
+++ src/main/java/plantapi/controller/UserController.java
@@ -4,13 +4,11 @@ import sqlconnection.SQLConnector;
 
 import java.sql.SQLException;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class UserController {
